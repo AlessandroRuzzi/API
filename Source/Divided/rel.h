d04d496f739df5a62919a7c16dd9847a03831c32
Alessandro Ruzzi <alrxruzzi98@gmail.com>
2019-09-18 17:36:52 +0200

    Create rel.h

---
+++ Source/Divided/rel.h
@@ -0,0 +1,12 @@
+#ifndef PROGETTO_API_REL_H
+#define PROGETTO_API_REL_H
+
+typedef struct rel {
+
+    char function[7];
+    char type[35];
+    char first[35];
+    char second[35];
+}rel;
+
+#endif //PROGETTO_API_REL_H
